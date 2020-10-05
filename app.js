215d6c521a53b5349ee25b549d75209d1cabe108
Shouraya <shourayagoyal2406@gmail.com>
2020-10-05 12:26:38 +0530

    routing done & error 404 added

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));  //PUBLIC --> static files (css)
 
 app.get("/", function(req,res){
+	res.redirect("/clone");
+});
+
+app.get("/clone", function(req,res) {
 	res.render('clone');
+});
+
+app.get("*", function(req, res){
+	res.render('error');
 });
 //Server 
 var port = process.env.PORT || 3000;
