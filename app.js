2cc8b7b5bd00e8e09164cecfcf5de2eb5351013f
Shouraya <shourayagoyal2406@gmail.com>
2020-10-05 11:42:34 +0530

    added ejs, css and js file and appropriate route

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));  //PUBLIC --> static files (css)
 
-
+app.get("/", function(req,res){
+	res.render('clone');
+});
 //Server 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
